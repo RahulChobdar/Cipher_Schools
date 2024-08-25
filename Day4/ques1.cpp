1ff612574b6090ce07a855182201c391e38f6b7d
Rahul <rahulchobdarpersonal@gmail.com>
2024-08-25 10:56:48 +0530

    "Day4"

---
+++ Day4/ques1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter first integer:";
+    cin>>a;
+    cout<<"Enter second integer:";
+    cin>>b;
+    cout<<"Addition: "<<a+b<<endl;
+    cout<<"Subtraction: "<<a-b<<endl;
+    cout<<"Multiplication: "<<a*b<<endl;
+    if(b!=0){
+        cout<<"Division: "<<a/double(b)<<endl;
+    }
+    else{
+        cout<<"Cannot be divided by 0"<<endl;
+        cout<<"Modulus: "<<a<<endl;
+        return 0;
+    }
+    cout<<"Modulus: "<<a%b<<endl;
+    return 0;
+}
---
+++ Day4/ques2.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Complex {
+private:
+    double real;
+    double imag;
+
+public:
+    Complex(double r, double i) : real(r), imag(i) {}
+    Complex operator+(const Complex& other){
+        return Complex(real + other.real, imag + other.imag);
+    }
+    Complex operator-(const Complex& other){
+        return Complex(real - other.real, imag - other.imag);
+    }
+    void display(){
+        cout << real << " + " << imag << "i";
+    }
+};
+
+int main() {
+    Complex num1(3, 4);
+    Complex num2(1, 2);
+    Complex sum = num1 + num2;
+    Complex diff = num1 - num2;
+    cout << "Complex Number 1: ";
+    num1.display();
+    cout << std::endl;
+    cout << "Complex Number 2: ";
+    num2.display();
+    cout << std::endl;
+    cout << "Sum: ";
+    sum.display();
+    cout << std::endl;
+    cout << "Difference: ";
+    diff.display();
+    cout << std::endl;
+    return 0;
+}
